8a3a2708a26ffc638e90763b25263e109c629c94
olser <gritsienko.oleg@mail.ru>
2022-08-31 12:51:38 +0300

    почти готово ( ну почти ) ещё мучаюсь с правильным selectedLIst)

--- src/app/js/module/newM.js
+++ src/app/js/module/newM.js
@@ -2,6 +2,45 @@ import debounce from "./debounce";
 
 export function myFFF() {
 
+
+  //delete
+  // const arr = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+  // let el = 3
+  // console.log('ssmae myIncludes', myIncludes(arr, el))
+
+
+  let selectedArr = []
+
+
+  function showMessageError(message) {
+    const errBlock = createEl('div', {className: 'error', textContent: `${message}`})
+    const searchInp = document.querySelector('.search__form')
+    searchInp.after(errBlock)
+
+    setTimeout(() => {
+      errBlock.remove()
+    }, 2500)
+  }
+
+  function myIncludes(arr, addingEl) {
+    if (arr.length === 0) return false
+    for (let innerArrEl of arr) {
+      // если innerArrEl имя === clicked имя
+      console.log('innerArrEl', innerArrEl)
+      if (innerArrEl[0] === addingEl) return true
+    }
+    return false
+  }
+
+  function getAddEl(arr, addingEl) {
+    if (arr.length === 0) return null
+    for (let innerArrEl of arr) {
+      // если innerArrEl имя === clicked имя
+      if (innerArrEl[0] === addingEl) return innerArrEl
+    }
+    return null
+  }
+
   console.log('very worksc!')
 
   function cleanInp() {
@@ -20,71 +59,88 @@ export function myFFF() {
   }
 
   const createEl = (tag, opts, fuForEvListener = null) => {
-    const el = document.createElement(tag)
+    const addingEl = document.createElement(tag)
     // перебираем ключи объекта и записывает соответствующие свойства в элемент
     for (const key in opts) {
-      el[key] = opts[key]
+      addingEl[key] = opts[key]
     }
 
     // if (fuForEvListener !== null) {
-    //   el.addEventListener('click', () => {
+    //   addingEl.addEventListener('click', () => {
     //     fuForEvListener()
     //   })
     // }
 
     // возвращаем готовый элемент
-    return el
+    return addingEl
   }
-
-  //
-  // function sendToSelected(el) {
-  //   console.log('el', el)
-  //   console.log(el.srcElement)
-  //   const moveingEl = el.srcElement
-  //   (moveingEl)
-  // }
 
   function showSearched(arr) {
     // arr - массив из [name, owner, stars] * 5
 
     //формирую searchList
     const searchedList = createEl('ul', {className: 'search__list'})
-    for (const el of arr) {
-      const listItem = createEl('li', {className: 'search__list-item', textContent: el[0]})
+    for (const addingEl of arr) {
+      const listItem = createEl('li', {className: 'search__list-item', textContent: addingEl[0]})
       searchedList.appendChild(listItem)
     }
+    document.querySelector('.search').appendChild(searchedList)
 
     //вешаю кликер для переноса элементов в selected
     searchedList.addEventListener('click', (e) => {
-      console.log('searched el ', e)
+
+      // добавляю в данные
+      if (myIncludes(selectedArr, e.target.innerText)) {
+        return 'repeat of adding elem'
+      }
+      // else find this e elem
+      const addingEl = getAddEl(arr, e.target.innerText)
+
+      console.log('addEl', addingEl)
+      // console.log('hmm', addingEl[0])
+
+
+      console.log('searched addingEl ', e)
       let selectedList = document.querySelector('.selected__list')
-      if(selectedList === null) { // не существ listNode
+      if (selectedList === null) { // не существ listNode
         selectedList = createEl('ul', {className: 'selected__list'})
         document.querySelector('.search').after(selectedList)
       }
 
-      arr.forEach(el => {
-        if (e.target.innerText === el[0]) {
-          console.log(el)
-          const emptyReadyItem = getSelectedNode()
-          const forSelectedItem = emptyReadyItem.firstElementChild
-          // emptyReadyItem.appendChild(forSelectedItem)
-          // for (const itm of el) {
-            forSelectedItem.appendChild(createEl('p', {textContent: `Name: ${el[0]}` }))
-            forSelectedItem.appendChild(createEl('p', {textContent: `Owner: ${el[1]}`}))
-            forSelectedItem.appendChild(createEl('p', {textContent: `Stars: ${el[2]}`}))
+      const emptyReadyItem = getSelectedNode()
+      const forSelectedItem = emptyReadyItem.firstElementChild
+
+      forSelectedItem.appendChild(createEl('p', {textContent: `Name: ${addingEl[0]}`}))
+      forSelectedItem.appendChild(createEl('p', {textContent: `Owner: ${addingEl[1]}`}))
+      forSelectedItem.appendChild(createEl('p', {textContent: `Stars: ${addingEl[2]}`}))
+      // }
+      // emptyReadyItem.appendChild(forSelectedItem)
+      selectedList.appendChild(emptyReadyItem)
+
+
+      document.querySelector('.selected__list').addEventListener('click', (listElem) => {
+        // console.log('clicked', e)
+        let deletingEl = listElem.target.closest('.selected__del-btn')
+        console.log(deletingEl)
+        if (deletingEl) {
+          deletingEl.parentElement.remove()
+          selectedArr = selectedArr.filter(item => {
+            console.log("item", item, 'delEl', deletingEl.previousElementSibling.firstElementChild.textContent)
+            return !deletingEl.previousElementSibling.firstElementChild.textContent === item
+          }) // одинаковым с именем удаляемого - удалить
+          // if (selectedArr.length === 0) {
+          //   selectedList.remove()
           // }
-          // emptyReadyItem.appendChild(forSelectedItem)
-          selectedList.appendChild(emptyReadyItem)
         }
       })
+
 
       // e.target.remove()
       searchedList.remove()
       cleanInp()
 
     })
-    document.querySelector('.search').appendChild(searchedList)
+
     // const selectedList = document.querySelector('.selected__list')
     // selectedList.appendChild
 
@@ -93,36 +149,42 @@ export function myFFF() {
 
 
   async function getReps(inpValue) {
-    console.log('in getReps')
     const myQuery = `q=${inpValue}&sort=stars&order=desc&per_page=5`;
     // const myQuery = "q=tetris+language:assembly&sort=stars&order=desc";
 
     if (inpValue !== '') {
+
+
       const response = await fetch(
-        `https://api.github.com/search/repositories?${myQuery}`
-      );
+        `https://api.github.com/search/repositories?${myQuery}`)
+
+
+      // const response = await fetch(
+      //   `https://api.github.com/search/repositories?${myQuery}`
+      // );
+
       const reps = await response.json();
-      console.log('in getReps result', reps)
-      // reps.then(() => {
+      if (reps.hasOwnProperty('message')) { // костальная обработка 403
+        showMessageError('error of search. Try again')
+        return reps.message
+      }
       if (reps.total_count === 0) {
-        // show str: "Nothing was found" +/-
+        showMessageError('nothing found');
       } else {
 
-        showSearched(getSearched(reps.items))
+        // если запрос прошёл успешно -
+        // getSearched(reps.items)) - массив из 5 массивов [name, owner,stars]
+        console.log('result', showSearched(getSearched(reps.items)))
       }
       // })
       return reps
+
+
     }
 
 
   }
 
-  // function logInp(inpVal) {
-  //   console.log('inLogInp')
-  //   setTimeout(() => {
-  //     console.log('trying to emulate fetch')
-  //   }, 1000)
-  // }
 
   function getSearched(reps) {
     return reps.reduce((list, cur) => {
@@ -131,7 +193,7 @@ export function myFFF() {
     }, [])
   }
 
-  (function () {
+  (function () { // для запуска программы
 
     const inp = document.querySelector('.search__inp')
 // обернули в debounce
@@ -141,19 +203,12 @@ export function myFFF() {
 
     inp.addEventListener('input', async (e) => {
       //удаляю предыдущий автокомплит
-      const selectedList = document.querySelector('.search__list')
-      if (selectedList) selectedList.remove()
+      const searchedList = document.querySelector('.search__list')
+      if (searchedList) searchedList.remove()
 
       // запрос
       const reps = await debouncedLoginp(inp.value)
-      // createAutocomplit(reps)
     })
-    // фия запроса
-    // debounce(getReps(inp.value), debounceTime)
-    // .then(data => {
-    // inp.value = '' // если подтверждение
-    // console.log('in Listener result', data)
-    // })
 
 
   })()
